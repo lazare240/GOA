ea383090a9f851aa171f4af34a3a302da33f4803
lazare240 <modinaxesachxere325@gmail.com>
2025-03-21 23:21:21 +0400

    Commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,17 @@
+name = "lazare"
+surname="todaze"
+age="10"
+height="1.39"    
+
+knows_programming = True
+is_ugly = False
+
+isugly = False
+
+    
+love_football = "I love football very much and"
+
+study_hard = "I study hard"
+
+
+print("I am " + name + " " + surname +  " " +  age + " " + height + love_football + " " + study_hard) 
---
+++ day 04/classwork/classwork.py
@@ -0,0 +1,31 @@
+name = "groupe59"
+age = 100 
+float = 10.0
+
+name = "lazare"
+
+name = "niko"
+
+name = "gurami"
+
+name = "giorgi"
+
+
+print(name)
+
+food = "pizza"
+
+food = "xachapuri"
+
+food = "ice cream"
+
+food = "mwadi"
+
+print(food)
+
+name = "group59"
+print(10)
+age = 100
+print(10.0)
+print("goa best") 
+
